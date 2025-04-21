4e8f54aa4d17f50163cac3075be07efc249a1477
Hdvpdrm <hardcoderyan@gmail.com>
2025-04-21 13:20:01 +0300

    made cd and exit

---
+++ command_invoker.c
@@ -0,0 +1,103 @@
+#include"command_invoker.h"
+
+CommandPtr commands[] =
+  {
+    &cd,
+    &quit
+  };
+
+
+short cd(char** tokens, size_t size)
+{
+  if(size != 2)
+    {
+      strcpy(ash_error_buf,"cd command suppose to have 1 argument only!");
+      return ASH_INCORRECT_ARG_NUMBER;
+    }
+  if (!tokens[1])
+    {
+      strcpy(ash_error_buf, "Argument for cd command is NULL!");
+      return ASH_COMMAND_FAILURE;
+    }
+
+  char expanded_path[ASH_MAX_LENGTH];
+  if(expand_path(tokens[1],expanded_path) != OK) return ASH_COMMAND_FAILURE;
+  
+  if(chdir(expanded_path) == 0)
+    {
+      get_cwd();
+    }
+  else
+    {
+      strcpy(ash_error_buf,"failed to change directory!");
+      return ASH_COMMAND_FAILURE;
+    }
+  
+  return OK;
+}
+short quit(char** tokens, size_t size)
+{
+  return ASH_EXIT_SUCCESS;
+}
+
+short split_input(char** input, char*** output, size_t* size)
+{
+    if (!input || !*input) return ASH_NULL_POINTER;
+
+    size_t count = 0;
+    size_t capacity = 1; 
+
+    *output = (char**)malloc(capacity * sizeof(char*));
+    if (!*output) return ASH_MALLOC_FAILURE;
+
+    char* token = strtok(*input, " ");
+    while (token != NULL)
+      {
+        if (count >= capacity)
+	  {
+            capacity *= 2;
+            char** temp = (char**)realloc(*output, capacity * sizeof(char*));
+            if (!temp)
+	      {
+                for (size_t i = 0; i < count; i++)
+		  {
+                    free((*output)[i]);
+                }
+		
+                free(*output);
+                return ASH_MALLOC_FAILURE;
+            }
+            *output = temp;
+        }
+        (*output)[count] = strdup(token);
+        if (!(*output)[count])
+	  {
+            for (size_t i = 0; i < count; i++)
+	      {
+                free((*output)[i]);
+	      }
+            free(*output);
+            return ASH_MALLOC_FAILURE;
+	  }
+        count++;
+        token = strtok(NULL, " ");
+    }
+
+    *size = count;
+    return OK;
+}
+short invoke_command(char*** tokens,size_t size)
+{
+  if(!tokens || size == 0)
+    return ASH_NULL_POINTER; 
+  
+  char** arr = *tokens;
+  char* head = arr[0];
+  CommandPtr command = GET_COMMAND(head);
+  if(command == NULL)
+    {
+      return ASH_UNKNOWN_COMMAND;
+    }
+  
+  return command(arr,size);
+}
---
+++ command_invoker.h
@@ -0,0 +1,24 @@
+#ifndef COMMAND_INVOKER_H
+#define COMMAND_INVOKER_H
+#include"interface.h"
+#include"util.h"
+#include"state.h"
+
+typedef short (*CommandPtr)(char**,size_t size);
+extern CommandPtr commands[];
+
+#define GET_COMMAND(n)\
+  ( \
+    strcmp(n,"cd")   == 0? commands[0]:\
+    strcmp(n,"exit") == 0? commands[1]: \
+    NULL \
+    )\
+
+
+extern short cd(char** tokens, size_t size);
+extern short quit(char** tokens, size_t size);
+
+extern short split_input(char** input,char*** output, size_t* size);
+extern short invoke_command(char*** tokens,size_t size);
+
+#endif
